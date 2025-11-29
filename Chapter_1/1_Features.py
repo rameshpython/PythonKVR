12ac84caf59a9fb0365622b14fb9b90bad87b81a
rameshpython <ramababugaganam5515@gmail.com>
2025-11-29 19:48:05 +0530

    python Features

--- Chapter_1/1_Features.py
+++ Chapter_1/1_Features.py
@@ -3,48 +3,194 @@
 - Python is a combination of Object oriented and Function Oriented
 - test git from pycham
 - I am adding some more thing to commit------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
+
+
+"""
+Python Features:
+what is Features --> what special qualities in it like services OR facilities ,and those Features are used by 
+language programmers for developing real time Application.
+
+Python programming Features They  are :
+-------------------------------------
+1.simple
+2.platform independent
+3.Dynamically typed 
+4.Interpreted
+5.Freeware and open source
+6.High level
+7.Robust
+8.Functional(procedure  oriented)and object oriented
+9.Extensible
+10.Embedded
+11.supports Third party Modules such numpy, pandas, Matlotlib...etc
+
+
+ 
+
+1.simple --->
+--------
+Python is a simple programing language because of Three important Technical Features 
+They are:
+1.Module : Any file(.py) having  of classes, functions, and variables are called module  
+example : math, cmath, calender, these are modules of python 
+
+2.Garbage collector : When we create variables or objects ,They use memory in 'RAM' .when those variables are no longer 
+needed the Garbage collector automatically removes space to them and delete's.only the delete string character available 
+space is gives to it.So the Garbage collector is Automatically works in python.
+
+3.python programing lang provides User-friendly syntaxes.less lines and less time it takes.
+
+========================================================================================================================
+
+2.platform Independent--->
+----------------------
+python is a plotform Independent programming language because The python program works on all operating systems(OS).
+
+In plotform Independent created some programing languages that some programing languages works on any OS ,without 
+changinng anything,Because they  have interpreter like JVM and PVM they take souce code to intermediate code and by 
+using PVM translate to the machine understandable language (1,0) Binary language.
+
+========================================================================================================================
+
+3.Dynamically Typed--->
+In this we have two types they are :
+1.static Typed programing lang
+2.Dynamically Typed programing language
+
+->static Tyeped : when we create a variable and gives the value to that variable At that time only we mention that variable 
+type and give the value then only the value get,otherwise we get Error .This is called static Typed programing lang.
+Example : int a = 10 
+          int b = 20 
+          int c = a+b
+          here we declare the variables ,that is which data type is.
+          
+->Dynamically Typed : We Don't need to mention any data type to the variables python automatically converts the value 
+to it's related Data type.So the python is Automatcally converting variable given value into it's type of datatype.
+This is called Dynamically Typed programming language.
+Example: a = 10 
+         b = 20
+         c = 2.4
+         d = a+b+c
+    
+========================================================================================================================
+
+3.Interpreted programming---> 
+-------------------------
+When we develop amy  python program ,we must give some file name with an extension (.py) filename.py).
+--python is interpreted programing language ,It can runs on any operating systems.  
+--When we execute python program ,two process taken place internqally
+a)compilation process
+b)Execution process
+
+(a)->compilation process: source code (python programming)use as(.py) python having for this soure code one python compiler
+it reads line by line to check the mistakes.And sends to the byte code.The source code converted to byt code it is called
+Intermediate code.
+
+(b)->Excecution process:When we taken the souce code into bytecode (.pyc).At that time PVM works on bytecode read 
+line by line and send it to the machine understandable language ,computer language (0,1)binary code which is understandable
+by OS and processor and gives the Result.
+
+
+4.Freeware and open source--->
+--------------------------
+Freeware : Freeware means the software is free to use ,but we can not see or change the source code.
+-Download it for free
+-Install and use it without paying
+-we can not see program code(source code)
+-we can not modify  the code
+-we can not Redistribute modify the code
+
+Freeware examples:
+-Google chrome
+-zoom free 
+
+Open source : open source means the source code is open for everyone to see,use,modify,and share
+-you can see the full code 
+-modify and improve it
+-share your modification 
+-use it in your own projects 
+
+
+5. High level programming--->
+-------------------------
+1.low level  programming languages
+2.High level programmig languages
+
+low level : computer understandable  language
+
+high level : high level programming language is used in programs (source code) .it is user frienly languages. 
+
+
+
+6.Robust(strong)--->
+---------------
+Robust means strong,stable,and error tolerate ,so  python can handle error,safely, avoid crashes, and keep the program 
+running smothly.
+Robust is use to not crash the program and it use the "Exception handling" 
+Exception handling -- the  process converting technical error message into user-friendly error message is called 
+"exception handling".
+
+
+
+7.Extensible--->
+------------
+Extensible means python provided Built-in modules to use other programing languages, And this type of providing service  
+to others called Extensible.
+
+8.Embedded--->
+----------
+Embedded means python programming will uses other language sevices/libraries also.In other words ,we call other language 
+code inside of python program and hence python programing lang is Embedded programing language.
+ 
+ 
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
