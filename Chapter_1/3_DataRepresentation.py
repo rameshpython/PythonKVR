8b25f4240be51739eb3b366215f47cfe0852a924
rameshpython <ramababugaganam5515@gmail.com>
2025-12-02 17:54:30 +0530

    Data Representation./ literals(values),Identifiers(variables),structured and unstructured ,Data science.

---
+++ Chapter_1/3_DataRepresentation.py
@@ -0,0 +1,107 @@
+"""
+------------------
+Data Representation:
+-------------------
+-->What is Data
+---------------
+-The processed information is called Data.
+-The purpose of collection data is that " To take effective Decisions".
+-The data always stored in main memory 'RAM',PROCESSED in main memory and results displayed on the monitor.
+-And to store data permanently we use secondary memory hard disk,DVD,pen drives etc.
+-The structured format is called as DATA ,  Whereas unstructured format is called information.
+
+-->what  is structured and unstructured data:
+--------------------------------------------
+-One purpose of 'Data science' is to  structure Data,making it iterable(meaningful) and easy to work with it .
+
+-Data can be categorized into two types:
+->structured data
+->unstructured data
+
+->unstructured data
+ ------------------
+ -unstructured data is not organized ,we must organize the data for analysis purposes.
+ -it is not in position of  correct form to use .it is just information but not it is in positional.
+ ex: O   0
+     O O 0
+     0 0 0
+       0
+
+ ->structured data
+   ---------------
+-structured data is organized and easier to work with.
+ex: 0 0 0
+    0 0 0
+    0 0 0
+
+-->How to structure a Data?
+-We can use an Array or a database table (rows and columns) to structure or present data.
+
+example of am array:
+-------------------
+[80,36,78,98,87,94,92,123,342,125]
+
+-The following example shows how to create an Array in python.
+
+
+
+-->Types of literals or values:
+------------------------------
+->In python programming ,Broadly ,literals or values classified into 5 Types.They are
+1.Integer literals
+2.string literals
+3.float literals
+4.Boolean literals
+5.collection literals
+
+->All the literals are called "Data" and need to store in main memeory.
+
+
+-->Importance of Identifiers OR variables
+-----------------------------------------
+- In any programming language literals or values Must be stored in main memory by allocating sufficient amount
+of memory space with the help of Data types.
+-for storing value we need space for that value , We giving one identity to store value, that  storing name called as
+variable
+-we know that all types of literals are stored in main memory by having memory space .
+-To process value which are present in memory space ,programmers must give DISTINCT NAMES to the created memory spaces
+that DISTINCT NAME is called identifier.
+-The identifier values are changing during program execution and hence identifiers are also called variables.
+-Hence all types of literals are stored in the form variables and all variables are called objeccts.
+
+-->Definition of variable:
+-------------------------
+-A variable is an identifier whose value can be changed during program Execution.
+-when we give the variable and its value ,the value of variable we can change and assign to the same variable another
+values also.
+-In the place of variable we mention our way of name to the variable .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-->
+
+
+
+
+
+"""
