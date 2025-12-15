98318f171b33cd6075e9e73c665bba80295ad1d3
rameshpython <ramababugaganam5515@gmail.com>
2025-12-15 10:04:52 +0530

    String Data types

---
+++ Chapter_2/StringDatatypes.py
@@ -0,0 +1,57 @@
+s1 = "python"
+print(s1,type(s1))
+
+s2 = 'python'
+print(s2,type(s2))
+
+s3 ="A"
+print(s3,type(s3))
+
+s4 = 'A'
+print(s4,type(s4))
+
+s1 =  "12345"
+print(s1,type(s1))
+
+s2 = "python23.56"
+print(s2,type(s2))
+
+
+s3 = "123$456_abc"
+print(s3,type(s3))
+
+
+s4 = "123$#$%&8912"
+print(s4,type(s4))
+
+s = "python programming"
+print(s,type(s))
+
+s1 = "123hello 2+5j,True,567,34.56"
+print(s1,type(s1))
+
+p = """python programming 
+       created by Guido an
+       Rossum F NO:4-5,hill side
+       python software foundation
+       nether lands-34"""
+print(p)    #Double,thrible quotes give the paragraph type Answers
+
+a = """hello world how are you"""
+print(a,type(a))
+
+a1 = """A"""
+print(a1)
+
+q = '''python programming 
+       created by Guido an
+       Rossum F NO:4-5,hill side
+       python software foundation
+       nether lands-34'''
+print(q)    #Single ,thiriple quotes gives also same paragraph type answers
+
+b = '''hello python programming how are you'''
+print(b,type(b))
+
+b1 = '''A'''
+print(b1)
