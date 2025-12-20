03a616efa3fa57099212d1082e03d7c4a45eac63
rameshpython <ramababugaganam5515@gmail.com>
2025-12-20 12:28:01 +0530

    String Example is added

---
+++ Chapter_2/StringExample.py
@@ -0,0 +1,18 @@
+
+x = "john ,5 class,2 rank,530 marks,st.xavier_high_school"
+#x1 = x.split()
+#print(x,len(x1))
+
+
+a = x.split()
+print("student name:",a[0]) #name of student
+print("student marks:",a[3]) #student marks
+#print("school name:",a[4])  #school name
+print("How many words in a:",a,len(a))  #how many words are there in x
+b = x.split(",")  # split(,)
+print(b)
+c = len(x)
+print("how many characters :",c) #how many characters are there
+
+
+
