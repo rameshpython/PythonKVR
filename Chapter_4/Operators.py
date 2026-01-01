5417469924a75b09fee3d2a8d948a4454961e785
rameshpython <ramababugaganam5515@gmail.com>
2026-01-01 19:13:27 +0530

    operators were started

---
+++ Chapter_4/Operators.py
@@ -0,0 +1,30 @@
+
+
+'''
+1. Arithmetic Operators
+2. Assignment Operator
+3. Relational Operators (Comparision )
+4. Logical Operators (Comparision )
+5. Bitwise Operators--Most IMP
+6. Membership Operators
+	    a) in operator
+		b) not in Operator
+7. Identity Operators
+		a) is operator
+		b) is not Operator
+'''
+
+'''
+SLNO		SYMBOL			MEANING			EXAMPLES :  a=10  b=3
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+1.			    +				Addition			print(a+b)------------13
+2.			    -				Substraction			print(a-b)------------ 7
+3.			    *				Multiplication		print(a*b)------------30
+4.			    /				Division				print(10/3)---------->3.3333333333335
+							(Float Quotient)
+5.		        //				Floor Division		print(10//3)--------->3
+							(Integer Quotient)
+6.			   %				Modulo Division		print(10%3)----------->1 
+							(Remainder)
+7.			   **				Exponentiation		print(a**b)----------->1000
+'''
